18112cf9e3b6b45d637fce99575bc6bfa4e64cde
Andrew Tristan Parli <drewparli@gmail.com>
2018-01-10 00:02:18 +0100

    normal update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ d3.select(window).on('load', main);
    See https://github.com/d3/d3-queue for more details. */
 function main(handsJSON) {
   d3.queue()
-    .defer(d3.json, "data-hourly.json")  // police districts
+    .defer(d3.json, "./data/data-hourly.json")  // police districts
     // .defer(d3.json, "sf_crime.geojson")  // criminal occurrences
     .await(function(errMsg, data1) {
     // .await(function(errMsg, data1, data2) {
@@ -25,8 +25,9 @@ function main(handsJSON) {
 var vis = {
     "barchart_1": {
         "obj": null,
-        "width": null,
-        "height": null
+        "width": 400,
+        "height": 200,
+        "bar_width": 10
     }
 }
 
@@ -49,13 +50,51 @@ function initBarChart1(hour) {
     .attr("width", vis.barchart_1.width)
     .attr("height", vis.barchart_1.height)
 
-  vis.barchart_1.obj.selectAll(".stepHour")
+  // add a group for each time-step
+  vis.barchart_1.obj.selectAll("g")
     .data(hour.data)
-
     .enter()
     .append("g")
     .attr("id", function(d) { return d.date.id })
     .classed("step", true)
+    .append("rect")
+    .attr("id", "CO")
+    .append("rect")
+    .attr("id", "CO2")
+    .append("rect")
+    .attr("id", "CO3")
+    .append("rect")
+    .attr("id", "CO4")
+
+  // add the CO bar for each time-step
+  vis.barchart_1.obj.selectAll(".step")
+    .data(hour.data)
+    .append("svg:rect")
+    .classed("CO", true)
+    .attr("width", 10)
+    .attr("height", 20)
+    .attr("x", null)
+    .attr("y", null)
+    .style("fill", "blue")
+
+  // add the O3 bar for each time-step
+  vis.barchart_1.obj.selectAll(".step")
+    .data(hour.data)
+    .append("svg:rect")
+    .classed("O3", true)
+    .attr("width", 10)
+    .attr("height", 20)
+    .style("fill", "green")
+
+  // add the NOX bar for each time-step
+  vis.barchart_1.obj.selectAll(".step")
+    .data(hour.data)
+    .append("svg:rect")
+    .classed("NOX", true)
+    .attr("width", 10)
+    .attr("height", 20)
+    .style("fill", "red")
+
 }
 
 
