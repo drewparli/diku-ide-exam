ceb91518d0dcf06e2f0bba183e090595acca86fc
Viktorija Nekrasaite <t-vinekr@microsoft.com>
2018-01-12 10:46:45 +0100

    Date and Time pickers

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ function main(handsJSON) {
         // visualize(data1, data2)
       }
     })
+
+  d3.select("#time").on("input", updateTime);
 }
 
 
@@ -156,6 +158,10 @@ function handle_time_step() {
 
 }
 
+function updateTime() {
+  var id = d3.select("#time").node().value;
+  d3.select("#time_txt").text(getTimeByIndex(id));
+}
 
 /* Some useful helper functions */
 function step_n_times(n) {
@@ -169,4 +175,12 @@ function encodeDateID(date) {
 
 function decodeDateID(dateID) {
   return dateID.slice(1)
+}
+
+function getTimeByIndex(id) {
+  var data_all = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", 
+	              "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+	              "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",
+	              "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"];
+  return data_all[id];
 }
