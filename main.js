ee05c6c6b149602ef6bf6bc7900d285dc61670e4
Andrew Tristan Parli <drewparli@gmail.com>
2018-01-12 12:58:13 +0100

    added transition and auto-play to barchart

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ d3.select(window).on('load', main);
 function main(handsJSON) {
   d3.queue()
     .defer(d3.json, "./data/data-hourly-2.json")  // police districts
-    // .defer(d3.json, "sf_crime.geojson")  // criminal occurrences
-    .await(function(errMsg, data1) {
-    // .await(function(errMsg, data1, data2) {
+    .defer(d3.json, "./data/date-index.json")
+    // .await(function(errMsg, data1) {
+    .await(function(errMsg, data1, data2) {
       if (errMsg) {
         console.log("ERROR: " + errMsg)
       } else {
-        visualize(data1)
-        // visualize(data1, data2)
+        // visualize(data1)
+        visualize(data1, data2)
       }
     })
 }
@@ -23,15 +23,18 @@ function main(handsJSON) {
 // These objects hold all the magic numbers and elements that need to be
 // accessed globally for the entire visualization.
 var vis = {
-  "current_step": "2017-12-31 22:30",
+  "current_step": "2017-12-31 20:00",
   "data": {
     "max_value": null,
     "min_value": null,
-    "max_percent": 10,
-    "min_percent": -10
+    "max_percent": 2,
+    "min_percent": -2,
+    "raw": null,
+    "dateIndex": null
   },
   "default": {
-    "date": "2017-12-31 22:30"
+    "date": "2017-12-31 20:00",
+    "data": null
   },
   "barchart_1": {
     "obj": null,
@@ -47,17 +50,21 @@ var vis = {
 
 // This function organizes the entire visualization.
 // It is called after the data files are loaded.
-function visualize(dataHourly) {
+function visualize(dataHourly, dateIndex) {
   // Just a sanity check
-  console.log("DataSet", dataHourly)
+  // console.log("DataSet", dataHourly)
 
   // Build up the initial state of the map visualization.
-  initBarChart1(dataHourly)
+  vis.data.raw = dataHourly
+  vis.data.dateIndex = dateIndex
+  vis.default.data = dateIndex[vis.default.date]
+
+  initBarChart1(dataHourly, dateIndex)
 }
 
 
 // This function sets up the main svg element for the map visualization
-function initBarChart1(hour) {
+function initBarChart1(hour, dateIndex) {
 
   let min = vis.data.min_percent
   let max = vis.data.max_percent
@@ -65,6 +72,7 @@ function initBarChart1(hour) {
   let height = vis.barchart_1.height
   let margin = vis.barchart_1.margin
   let pad = vis.barchart_1.barpad
+
 
   // Setup all the scales that will be needed in the visualization
   let xScale = d3.scaleBand()
@@ -91,15 +99,15 @@ function initBarChart1(hour) {
   // Add a group for each time-step to organize the bars
   barchart.select("#time-steps")
     .selectAll("g")
-    .data(hour.data)
+    .data([vis.default.data], function(d) {console.log(d)} )
     .enter()
     .append("g")
-    .attr("id", function(d, i) { return encodeDateIDAttr(d.date.id) })
+    .attr("id", encodeDateIDAttr(vis.default.date))
     .classed("timeStep", true)
 
     // At each time step we now add the bars for each particle type
     .selectAll("rect")
-    .data(function(d,i) { return d.particles })
+    .data(function(d,i) { return d })
     .enter()
     .append("rect")
     .attr("id", function(d,i) { return d.name })
@@ -110,18 +118,20 @@ function initBarChart1(hour) {
       })
     .attr("width", xScale.bandwidth())
     .attr("height", function(d) { return yScale(Math.abs(d.percent)) - 0.5 * height })
-    .attr("class", function(d) {
-      if (this.parentNode.id == encodeDateIDAttr(vis.default.date)) { return "showBar"}
-      else { return "hideBar" }
-    })
+    // .attr("class", function(d) {
+    //   if (this.parentNode.id == encodeDateIDAttr(vis.default.date)) { return "showBar"}
+    //   else { return "hideBar" }
+    // })
+    .attr("class", function(d) { return d.name })
+    .classed("bar", true)
 
-
-  // Add the x and y axes
+  // Add the x axis
   barchart.append("g")
     .attr("id", "x")
     .attr("transform", "translate(20, 100)")
     .call(d3.axisBottom(xScale))
 
+  // Add and edit the y axis
   barchart.append("g")
     .attr("id", "y")
     .attr("transform", "translate(20, 0)")
@@ -133,32 +143,47 @@ function initBarChart1(hour) {
     .attr("x2", width)
     .style("stroke", "rgba(0,0,0,0.2)")
 
-  barchart.on("click", handle_time_step)
+  // barchart.on("click", handle_time_step, 5)  // one step forward
+  barchart.on("click", handle_it)  // use this for auto play
 
+}
+
+function handle_it() {
+  handle_time_step()
+  d3.interval(handle_time_step, 1500)
 }
 
 
 function handle_time_step() {
   // cause a transformation to the next timestep
-  console.log(this, d3.event, vis.current_step)
+  let yScale = vis.barchart_1.yScale
+  let height = vis.barchart_1.height
+  // console.log(this, d3.event, vis.current_step)
   current = vis.current_step
   next = step_n_times(1)
 
   // hide the current time step
-  vis.barchart_1.obj.select(encodeDateIDSelector(current))
+  bars = vis.barchart_1.obj.select(encodeDateIDSelector(current))
     .selectAll("rect")
-    .classed("showBar", false)
-    .classed("hideBar", true)
 
-  // show the next time step
-  vis.barchart_1.obj.select(encodeDateIDSelector(next))
-    .selectAll("rect")
-    .classed("hideBar", false)
-    .classed("showBar", true)
+  bars.transition()
+    .duration(800) // in milliseconds
+    .attr("y", function(d,i) {
+      console.log(d, i, vis.data.dateIndex[next][i]["percent"])
+      let newPercent = vis.data.dateIndex[next][i]["percent"]
+      if (newPercent < 0) { return yScale(0) }
+      else { return yScale(0 - newPercent) }
+    })
+    .attr("height", function(d,i) {
+      let newPercent = vis.data.dateIndex[next][i]["percent"]
+      console.log(newPercent, yScale(Math.abs(newPercent)) - 0.5 * height, vis.current_step)
+      return yScale(Math.abs(newPercent)) - 0.5 * height
+    })
+
+  vis.barchart_1.obj.select(encodeDateIDSelector(current))
+    .attr("id", encodeDateIDAttr(next))
 
   vis.current_step = next
-
-  console.log(current, next, vis.current_step)
 }
 
 
